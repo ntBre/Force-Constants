56dd66037eefeb4d541e64bef07a193a47fdb660
Brent Westbrook <r2518@maple.cm.cluster>
2019-08-25 16:50:27 -0500

    refactored to remove some of the prefixes and now calculate derivatives

--- main.jl
+++ main.jl
@@ -3,9 +3,10 @@ include("./Utilities.jl")
 # DONE check if there is space to write more files
 # DONE check if output file is finished
 # DONE capture energy from output
-# TODO perform calculation on energy and save
+# DONE perform calculation on energy and save
 # TODO refactor to submit one job at a time and remove pos and neg prefixes
 # 	This should make it easier to extend to higher dimensions
+#       Mostly done
 
 MAXFILES = 250
 
@@ -17,37 +18,23 @@ function firstderivative(labels, coords, delta, test=true)
     tempcoords = copy(coords)
     rm("./Input", recursive=true)
     mkdir("Input")
-    energies = []
+    refjob = makejob(0, labels, tempcoords, 50, test)
+    refout = submitjob(refjob, 0)
     toread = []
+    push!(toread, [0, refout])
+    energies = []
     for atom in 1:length(coords)
         for coord in 1:length(coords[atom])
-            # makenames
             posfilenum = parse(Int, string(atom) * string(coord))
             negfilenum = -parse(Int, string(atom) * string(coord))
-            poscomname = "input$posfilenum.com"
-            negcomname = "input$negfilenum.com"
-            pospbsname = "mp$posfilenum.pbs"
-            negpbsname = "mp$negfilenum.pbs"
-            # writefiles
             tempcoords[atom][coord] += delta
-            writecom(poscomname, labels, tempcoords, 50, test)
-            writepbs(posfilenum, test)
+            posout = submitjob(makejob(posfilenum, labels, tempcoords, 50, test), posfilenum)
             tempcoords[atom][coord] -= 2*delta
-            writecom(negcomname, labels, tempcoords, 50, test)
-            writepbs(negfilenum, test)
-            # submit jobs
-            run(`mv $poscomname ./Input/.`)
-            run(`mv $pospbsname ./Input/.`)
-            run(`mv $negcomname ./Input/.`)
-            run(`mv $negpbsname ./Input/.`)
-            cd("./Input")
-            posjobnum = read(`qsub $pospbsname`, String)
-            negjobnum = read(`qsub $negpbsname`, String)
-            println(posjobnum, negjobnum)
-            posout = "job$posfilenum.o$(posjobnum[1:5])"
-            negout = "job$negfilenum.o$(negjobnum[1:5])"
+            negout = submitjob(makejob(negfilenum, labels, tempcoords, 50, test), negfilenum)
+            println(posout, "\n", negout, "\n\n")
             push!(toread, [posfilenum, posout], [negfilenum, negout])
             if !test
+                cd("./Input/")
                 noroomtowrite = parse(Int64, read(pipeline(`ls`, `grep pbs`, `wc -l`), String)) > MAXFILES
                 nomoretowrite = atom == length(coords) & coord == length(coords[atom])
                 while (noroomtowrite || nomoretowrite) && length(toread) > 0
@@ -64,7 +51,9 @@ function firstderivative(labels, coords, delta, test=true)
             tempcoords[atom][coord] += delta
         end
     end
-    return energies
+    e0 = popfirst!(energies)[2]
+    derivatives = map(x -> [x[1], x[2] - e0 / delta], energies)
+    return derivatives
 end
 
 labels, coords = readxyz("geom.xyz")
